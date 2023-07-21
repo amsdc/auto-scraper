f421f2f3fcf9666d6c7c7a857f4bc851e20d67c2
Dhruv317 <droongta05@gmail.com>
2023-07-21 15:09:00 +0530

    Added Data Scraper

--- ai_scraper/llm_backend/gpt3_5_turbo.py
+++ ai_scraper/llm_backend/gpt3_5_turbo.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 from ai_scraper.llm_backend import ScraperGeneratorBase
-from typing import Optionl
+from typing import Optional
 from ai_scraper import html_helpers
 
 logger = logging.getLogger(__name__)
@@ -22,8 +22,33 @@ class OpenAIScraper(ScraperGeneratorBase):
         html_string = html_helpers.preprocess_html(self.url)
         self.html_string = html_string
 
-    def generate_scraper(self):
-        pass
+    def extract_data(self,k:Optional[int] = 30000):
+        total_data = []
+        for i in range(0,len(self.html_string),k):
+            #ADD TOO LONG INPUT THING
+            input_template = {
+                "spec_version": "1.0",
+                "page_url": self.url,
+                "page_html": str(html_string)[i:i+k],
+                "metadata": {
+                    "type": "tabular",
+                    "attributes": self.fields
+                }
+            }
+            
+            completion = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo-16k",
+                temperature = 0.0,
+                messages =  [{"role": "system", "content": prompt_template}, {"role": "user", "content": str(input_template)}]
+            )
+            
+            text = completion['choices'][0]['message']['content']
+            text = """{}""".format(text).replace("\\"," ").rstrip("`")
+            text = json.loads(text)
+            data = text["content"]
+            total_data.extend(data)
+        
+        return(total_data)
 
     def suggest_fields(self,k:Optional[int] = 30000):
         input_template = {
@@ -41,3 +66,4 @@ class OpenAIScraper(ScraperGeneratorBase):
         text = completion['choices'][0]['message']['content']
         print(text)
         return(text)
+
